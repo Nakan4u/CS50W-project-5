982715f746faa1d4cfe2d3a09762708334d5bc6f
Calum Bell <calumabell@googlemail.com>
2021-06-26 10:28:20 +0100

    migrated to postgresql database

--- backend/personal/settings.py
+++ backend/personal/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'personal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'personal-spa',
+        'USER': 'postgres',
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
